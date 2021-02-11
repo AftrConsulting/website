2ed2c35f7255bc1cc523818c10f3090b186ffbed
Tim Mouskhelichvili <t.mouskhelichvili@protonmail.com>
2021-02-11 11:54:48 -0500

    Fixed a few errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
-import Home from 'src/pages/home';
+import { NextSeo } from 'next-seo';
+import React, { ReactElement } from 'react';
+import { FirstSection } from 'src/componentsByPage/home/firstSection';
+import { Container } from 'src/components/container';
+import { useTranslation } from 'src/i18n';
+
+/**
+ * The Home component.
+ */
+const Home = (): ReactElement => {
+    const { t } = useTranslation([ 'common', 'home' ]);
+	
+    return (
+        <Container>
+            <NextSeo
+                title={t('title')}
+                description={t('description')}
+            />
+            <FirstSection />
+        </Container>
+    );
+};
+
+Home.getInitialProps = (): {} => ({
+    namespacesRequired: [ 'common', 'home' ]
+});
 
 export default Home;
