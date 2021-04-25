c642afd85fcfc4c20256968b18443dc7f244d4b6
Tim Mouskhelichvili <t.mouskhelichvili@protonmail.com>
2021-04-25 19:24:55 -0400

    fix(website): added french page

--- src/localizations/fr/home/index.ts
+++ src/localizations/fr/home/index.ts
@@ -1,159 +1,146 @@
-import { enHomeLocale } from 'src/localizations/en/home';
-
-const frHomeLocale: typeof enHomeLocale = {
+const frHomeLocale = {
     description: {
-        subTitle: 'Who We Are',
-        text1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-				Donec rhoncus sodales purus, vitae mollis dui interdum vitae. Donec rhoncus sodales purus, vitae 
-				mollis dui interdum vitae.  Donec rhoncus sodales purus, vitae mollis dui interdum vitae. 
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus sodales purus, vitae mollis dui interdum vitae. 
-				Ut viverra turpis non tortor sollicitudin convallis.`,
-        text2: `Duis suscipit risus ut justo iaculis tempus. Quisque luctus metus lacus, 
-				ultrices consequat turpis dignissim id. Nulla justo nisl, blandit ut posuere 
-				ac, pulvinar eget mi.`,
-        title: 'In hac habitasse platea dictumst. Ut posuere ante in est dapibus lacinia.'
+        subTitle: 'Qui Sommes Nous',
+        text1: `Nous sommes AftrConsulting, une équipe avec une passion pour aider nos clients à transformer leurs entreprises 
+				en créant d'excellentes applications Web. Nous proposons une vaste gamme de services dans le domaine du Web, 
+				du développement Web aux consultations et au coaching. Notre équipe se consacre à fournir 
+				les meilleurs services Web, toujours rapides et abordables.`,
+        text2: 'Nous ne vendons pas seulement des applications Web, nous créons des applications Web qui vendent.',
+        title: 'Nous aidons les entreprises à se réinventer.'
     },
     expertises: {
         bookConsultation: {
-            description: `Donec rhoncus sodales purus, vitae mollis dui interdum vitae. 
-						  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-						  Donec rhoncus sodales purus, vitae mollis dui interdum vitae.`,
-            title: 'Duis suscipit risus ut justo iaculis tempus.'
+            description: `Prenez un appel gratuit pour discuter de ce que nous pouvons faire pour votre projet.
+						  Nous pourrons répondre à toutes vos questions et vous fournir un prix adapté à votre budget.`,
+            title: 'AFTRConsulting délivre toujours.\u00A0🤝'
         },
         list: [
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll develop a great modern web application for your business.',
+                title: 'Web Development'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll optimize your site to 100% in PageSpeed Insights and fix other performance issues.',
+                title: 'Web Performance & Optimization'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll help you generate more leads and customers from your website.',
+                title: 'Conversion Rate Optimization'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll help you improve the quantity and quality of your website traffic from Google.',
+                title: 'Search Engine Optimization'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'Do you have a nasty bug that you cannot fix? We\'ll help you find the cause and solve it.',
+                title: 'Debugging'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll create an optimized landing page that will generate more leads/profits.',
+                title: 'Landing Page Creation'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll help you improve your code quality by implementing various tools and standards.',
+                title: 'Code Standards'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'Spaghetti code is hard to read and hard to maintain. We\'ll help you clean it up.',
+                title: 'Code Refactoring'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll help you find bugs earlier and save time and money by implementing unit tests.',
+                title: 'Unit Tests'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'We\'ll help develop, improve and maintain a WordPress website.',
+                title: 'WordPress'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'Some tools that we use: React, TypeScript, Webpack, JavaScript, GraphQL.',
+                title: 'Front-End Development'
             },
             {
-                description: 'Pellentesque laoreet nunc non enim rhoncus tristique. Nullam erat tortor, hendrerit.',
-                title: 'Lorem Ipsum'
+                description: 'Some tools and languages that we use: PHP, Node.js, Nginx, Docker, Kubernetes.',
+                title: 'Back-End Development'
             }
         ],
-        ps: 'Nulla dictum lacinia diam. Phasellus et pretium ligula. Ut elementum est quis congue eleifend.',
-        subTitle: 'Our Expertise',
-        title: 'Quisque suscipit tincidunt elit, ut ornare tortor.'
+        ps: 'Cette liste n\'est pas exhaustive, contactez-nous si vous avez besoin de quelque chose de spécial. Nous vous aiderons.',
+        subTitle: 'Nos Services',
+        title: 'Nous avons toutes les compétences nécessaires pour créer de superbes applications et sites web modernes.\u00A0🙏'
     },
     first: {
-        heading: '#1 web apps development',
+        heading: 'Services Développement Web',
         perks: [
-            'Lorem ipsum dolor sit amet',
-            'Lorem ipsum dolor sit amet',
-            'Lorem ipsum dolor sit amet',
-            'Lorem ipsum dolor sit amet'
+            'Développement Web Très Rapide\u00A0💪',
+            'Prix Abordable\u00A0💸',
+            'Délais Respectés\u00A0⌛',
+            'Meilleurs Résultats\u00A0🏆'
         ],
-        subHeading: 'we deliver the',
-        text1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae mauris ac lacus volutpat eleifend eu quis mi.',
-        text2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae mauris.'
+        subHeading: 'nous offront les meilleurs',
+        text1: `Nous créons des applications web qui vous permettront 
+				d'attirer de nouveaux clients et <span class="underline bold">d'augmenter vos profits</span>.`,
+        text2: 'Nous garantissons une satisfaction de 100% à nos clients.'
     },
     quotes: {
         reviews: [
             {
-                company: 'Company A',
-                description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam.
-							  Donec orci metus, tempor vitae varius in, porttitor 
-							  sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.
-							  Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam.
-							  Donec orci metus, tempor vitae varius in, porttitor 
-							  sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
+                company: 'Les Finances',
+                description: `We hired AFTRConsulting to redesign the back-end of our site,
+							  and we literally couldn't be happier with the results! 
+							  They are open to feedback, patient, communicative, and can adhere to a tight timeline. 
+							  We will definitely continue working with them in the future!`,
                 href: 'https://www.lesfinances.ca/',
                 img: 'matt.jpg',
                 name: 'Matt M.'
             },            
             {
-                company: 'Company B',
-                description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam.
-							  Donec orci metus, tempor vitae varius in, porttitor 
-							  sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.
-							  Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam.
-							  Donec orci metus, tempor vitae varius in, porttitor 
-							  sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
+                company: 'Psyhelp',
+                description: `When we started working with AFTRConsulting we never expected such great results. 
+							  Their customer service is top-notch and if we had a problem they were there to fix it quickly. 
+							  We recommend them to everyone that is looking for great web development services.`,
                 href: 'https://www.psyhelp.ca',
                 img: 'georges.jpg',
                 name: 'Georges M.'
             },
             {
-                company: 'Company C',
-                description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam.
-							  Donec orci metus, tempor vitae varius in, porttitor 
-							  sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.
-							  Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam.
-							  Donec orci metus, tempor vitae varius in, porttitor 
-							  sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
+                company: 'GoldenMTL',
+                description: `Thank you AFTRConsulting! Very good web development services! They helped me build a modern 
+							  website for my business that increased my visibility in Google, helped me attract new customers, 
+							  and increase my profits. I recommend it to everybody.`,
                 href: '',
                 img: 'jacky.jpg',
                 name: 'Jacky T.'
             }
         ],
-        subTitle: 'what our clients are saying',
-        title: 'Pellentesque laoreet nunc non enim rhoncus tristique.'
+        subTitle: 'ce que nos clients disent',
+        title: 'Nous garantissons une satisfaction de 100% à nos clients.\u00A0🤩'
     },
     services: {
         step1: {
-            description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam. Donec orci metus, tempor vitae varius in,
-			              porttitor sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
+            description: `Everything starts here. Together, we will create a custom-tailored flexible plan that will take
+			              all your requirements and questions into account. Also, you will get a quote adjusted to your budget.`,
             subTitle: '1. Consultation',
-            title: 'Quisque suscipit tincidunt elit, ut ornare tortor.'
+            title: 'We will evaluate your needs and create the best plan for YOU.\u00A0🥳'
         },
         step2: {
-            description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam. Donec orci metus, tempor vitae varius in,
-						  porttitor sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
+            description: `We develop using the agile methodology, so you will be able to observe the progress in real-time. 
+						  If needed, we can make adjustments on the fly therefore, the final product better reflects your vision.`,
             subTitle: '2. Development',
-            title: 'Quisque suscipit tincidunt elit, ut ornare tortor.'
+            title: 'We will develop the best modern web app for YOU.\u00A0😎'
         },
         step3: {
-            description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam. Donec orci metus, tempor vitae varius in,
-						  porttitor sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
-            subTitle: '3. Optimizations',
-            title: 'Quisque suscipit tincidunt elit, ut ornare tortor.'
+            description: `When the web app is deployed, we will make various optimizations to improve the website speed
+			              and conversion rate. Also, we will provide maintenance for your website.`,
+            subTitle: '3. Optimizations & Maintenance',
+            title: 'We will provide maintenance and optimizations for YOUR website.\u00A0😊'
         }
     },
     talkExpert: {
-        button: 'Get A Free Quote Now',
-        description: `Suspendisse mauris odio, commodo vitae sagittis a, feugiat quis quam. Donec orci metus, tempor vitae varius in,
-					  porttitor sed ex. Proin et interdum nisl. Nullam ut lorem ut mauris dictum finibus.`,
-        subTitle: 'Talk to our experts',
-        title: 'Quisque suscipit tincidunt elit, ut ornare tortor.'
+        button: 'Obtenir Une Estimation de Prix',
+        description: `Nous vous fournirons toutes les informations, répondrons à toutes vos questions et 
+		              vous donneront une estimation du prix gratuitement directement via notre chat.`,
+        subTitle: 'Contactez-nous',
+        title: 'Timide? 🙈 Parlez avec un expert en ligne.'
     }
 };
 
