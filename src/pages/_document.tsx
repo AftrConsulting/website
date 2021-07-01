7d8e664afbd69c320f89754e7c4f257628389e75
Tim Mouskhelichvili <t.mouskhelichvili@protonmail.com>
2021-07-01 11:01:42 -0400

    fix(website): added ahrefs verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -56,6 +56,7 @@ class MyDocument extends Document<IMyDocumentProps> {
                 <meta name={'apple-mobile-web-app-title'} content={configuration.general.company} />
                 <meta name={'apple-mobile-web-app-capable'} content={'yes'} />
                 <meta name={'apple-mobile-web-app-status-bar-style'} content={'default'} />
+                <meta name={'ahrefs-site-verification'} content={'afdd80cb0d012aad985a24b79cdd977ac16b2871756d10d4c0ca207bd0df4b9d'} />
                 <script type={'application/ld+json'} dangerouslySetInnerHTML={{ __html: getStructuredData() }} />
                 <AMPAnalyticsScript />
             </Head>
